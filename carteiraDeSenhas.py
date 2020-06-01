0c4268f5e51b7cf1d25bd68fd063f5a70ead8574
heitorzunta <G2e2C1i0A8n3I>
2020-06-01 19:58:12 -0400

    Design do programa e estutura de dados

---
+++ carteiraDeSenhas.py
@@ -0,0 +1,5 @@
+#! python3
+# carteitaDeSenhas.py - Armazena as senhas de contas num Dicionário e envia ao usuário por clippboard
+
+dicionario_senhas = {'email' : 'teste1234', 'blog': 'segredo3321', 'comum' : '1234567'}
+
