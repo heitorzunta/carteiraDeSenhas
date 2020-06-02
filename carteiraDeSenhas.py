9a10ecdfd831fa19d057affb1b4e6bddfd856857
heitorzunta <G2e2C1i0A8n3I>
2020-06-02 16:28:18 -0400

    programa completo

--- carteiraDeSenhas.py
+++ carteiraDeSenhas.py
@@ -1,5 +1,18 @@
-#! python3
+#! /usr/bin/env python3
 # carteitaDeSenhas.py - Armazena as senhas de contas num Dicionário e envia ao usuário por clippboard
+
+import sys, pyperclip
 
 dicionario_senhas = {'email' : 'teste1234', 'blog': 'segredo3321', 'comum' : '1234567'}
 
+
+if len(sys.argv) < 2:
+    sys.exit("Este programa necessita de um argumento. Ex. nomeDoprograma argumento")
+else:
+    conta = sys.argv[1]
+
+
+if conta in dicionario_senhas:
+    pyperclip.copy(dicionario_senhas[conta])
+else:
+   print("A conta {} não foi encontrada".format(conta))              
